a078973ed59c586d8e3c2ab9672a9765abed9a4a
rghai453 <54092064+rghai453@users.noreply.github.com>
2026-03-12 21:51:35 -0700

    WORK

--- src/components/ads/AdSenseScript.tsx
+++ src/components/ads/AdSenseScript.tsx
@@ -10,7 +10,7 @@ export function AdSenseScript(): ReactElement | null {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${publisherId}`}
       crossOrigin="anonymous"
-      strategy="lazyOnload"
+      strategy="afterInteractive"
     />
   );
 }
